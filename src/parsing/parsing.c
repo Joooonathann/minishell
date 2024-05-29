79bdf113e38cfa3ce7ed7bfb223fbb53f0d0130d
Emeric Krause <ekrause@k0r2p3.42mulhouse.fr>
2024-05-29 13:37:14 +0200

    parsing environment variables, command, options, arguments

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,21 +6,11 @@
 /*   By: ekrause <emeric.yukii@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 12:52:19 by ekrause           #+#    #+#             */
-/*   Updated: 2024/05/29 11:11:34 by ekrause          ###   ########.fr       */
+/*   Updated: 2024/05/29 13:31:19 by ekrause          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
-
-// int ft_strlen(char *str)
-// {
-// 	int i;
-
-// 	i = 0;
-// 	while (str[i])
-// 		i++;
-// 	return (i);
-// }
 
 int ft_strcmp(char *s1, char *s2)
 {
@@ -40,7 +30,7 @@ int ft_strcmp(char *s1, char *s2)
 	return (1);
 }
 
-int	command_is_valid(char	*command)
+int	command_is_valid(char *command)
 {
 	char	*commands[8];
 	int		i;
@@ -56,27 +46,85 @@ int	command_is_valid(char	*command)
 	i = 0;
 	while (commands[i])
 	{
-		if (ft_strcmp(command, commands[i]))
+		if (ft_strcmp(command, commands[i++]))
 			return (1);
-		i++;
 	}
 	return (0);
 }
 
+// void	parsing(char *prompt)
+// {
+// 	char	**tokens;
+// 	int		i;
+
+// 	tokens = ft_split(prompt, ' ');
+// 	i = 0;
+// 	if (tokens[i] && command_is_valid(tokens[i]))
+// 	{
+// 		printf("command: %s\n", tokens[i++]);
+// 		if (tokens[i] && ft_strcmp(tokens[0], "echo") && ft_strcmp(tokens[i], "-n"))
+// 			printf("option: %s\n", tokens[i++]);
+// 		while (tokens[i])
+// 			printf("arguments: %s\n", tokens[i++]);
+// 	}
+// 	else
+// 		printf("command not found: %s\n", tokens[0]);
+// }
+
+int	option_is_valid(char *option)
+{
+	char *options[3];
+	int i;
+
+	options[0] = "-n";
+	options[1] = "-e";
+	options[2] = NULL;
+	i = 0;
+	while (options[i])
+	{
+		if (ft_strcmp(option, options[i++]))
+			return (1);
+	}
+	return (0);
+}
+
+int	is_env_var(char *token)
+{
+	if (token[0] == '$')
+		return (1);
+	else
+		return (0);
+}
+
 void parsing(char *prompt)
 {
-	char	**tokens;
-	int		i;
+	char **tokens;
+	int i;
 
 	tokens = ft_split(prompt, ' ');
 	i = 0;
 	while (tokens[i])
 	{
-		printf("%s\n", tokens[i]);
+		if (is_env_var(tokens[i]))
+			printf("environment variable: %s\n", tokens[i]);
 		i++;
 	}
-	if (command_is_valid(tokens[0]))
-		printf("command valid\n");
+	i = 0;
+	if (tokens[i] && command_is_valid(tokens[i]))
+	{
+		printf("command: %s\n", tokens[i++]);
+		while (tokens[i] && option_is_valid(tokens[i]))
+			printf("option: %s\n", tokens[i++]);
+		while (tokens[i])
+			printf("argument: %s\n", tokens[i++]);
+	}
 	else
-		printf("command not valid\n");
+		printf("command not found: %s\n", tokens[0]);
+	i = 0;
+	while (tokens[i])
+	{
+		free (tokens[i]);
+		i++;
+	}
+	free (tokens);
 }

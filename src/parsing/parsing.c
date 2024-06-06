c4f967fbc494453c4609782abb4d42e877a6eb76
Emeric Krause <ekrause@k1r4p2.42mulhouse.fr>
2024-06-06 13:57:01 +0200

    tokenizer adjustement

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: ekrause <emeric.yukii@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 12:52:19 by ekrause           #+#    #+#             */
-/*   Updated: 2024/06/04 14:59:46 by ekrause          ###   ########.fr       */
+/*   Updated: 2024/06/06 13:52:55 by ekrause          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,43 @@ int	count_quotes(char *str, QUOTE quote_type)
 		i++;
 	}
 	return (j);
+}
+
+int	get_token_len(char **str)
+{
+	int		len;
+	bool	in_quote;
+	QUOTE	quote_type;
+
+	len = 0;
+	in_quote = false;
+	while (**str)
+	{
+		if ((**str == SIMPLE_QUOTE && count_quotes(*str, SIMPLE_QUOTE) > 1 && !in_quote) ||
+			(**str == DOUBLE_QUOTE && count_quotes(*str, DOUBLE_QUOTE) > 1 && !in_quote))
+		{
+			in_quote = true;
+			quote_type = (QUOTE) * *str;
+		}
+		else if (((**str == SIMPLE_QUOTE && quote_type == SIMPLE_QUOTE) ||
+				  (**str == DOUBLE_QUOTE && quote_type == DOUBLE_QUOTE)) &&
+				 in_quote)
+		{
+			(*str)++;
+			return (len);
+		}
+		else if (**str == ' ' && !in_quote)
+		{
+			(*str)++;
+			return(len);
+		}
+		else
+			len++;
+		(*str)++;
+	}
+	if (len > 0)
+		return (len);
+	return (0);
 }
 
 char	*tokenise(char **str)
@@ -76,13 +113,18 @@ char	*tokenise(char **str)
 
 void	parser(char *str)
 {
-	//t_tokens	*tokens;
-	char		*token;
-	
-	while ((token = tokenise(&str)) != NULL)
+	int	i;
+	while ((i = get_token_len(&str)) != 0)
 	{
-		printf("Token: %s\n", token);
-		free(token);
-		//ft_tokenadd_back(&tokens, ft_tokennew(token, 1));
+		printf("Token len:%d\n", i);
 	}
+	//t_tokens	*tokens;
+	// char		*token;
+	
+	// while ((token = tokenise(&str)) != NULL)
+	// {
+	// 	printf("Token: %s\n", token);
+	// 	free(token);
+	// 	//ft_tokenadd_back(&tokens, ft_tokennew(token, 1));
+	// }
 }

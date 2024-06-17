d9a1e4c77f2fe9aac574c882f531bd82b94272d2
Emeric Krause <ekrause@k0r2p3.42mulhouse.fr>
2024-06-17 16:01:59 +0200

    token structure

--- src/token_list/ft_tokenlast.c
+++ src/token_list/ft_tokenlast.c
@@ -6,22 +6,22 @@
 /*   By: ekrause <emeric.yukii@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 15:04:48 by ekrause           #+#    #+#             */
-/*   Updated: 2024/06/17 11:23:00 by ekrause          ###   ########.fr       */
+/*   Updated: 2024/06/17 14:02:38 by ekrause          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// t_tokens	*ft_tokenlast(t_tokens *tokens)
-// {
-// 	t_tokens	*temp;
+t_tokens	*ft_tokenlast(t_tokens *tokens)
+{
+	t_tokens	*temp;
 
-// 	temp = NULL;
-// 	if (tokens != NULL)
-// 	{
-// 		temp = tokens;
-// 		while (temp->next)
-// 			temp = temp->next;
-// 	}
-// 	return (temp);
-// }
+	temp = NULL;
+	if (tokens != NULL)
+	{
+		temp = tokens;
+		while (temp->next)
+			temp = temp->next;
+	}
+	return (temp);
+}

ad0842efebcf7561ea8ad6fea08c80a58a3cdea3
jalbiser <peliium.2022@gmail.com>
2024-07-02 11:48:36 +0200

    manager

--- src/minishell.c
+++ src/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ekrause <emeric.yukii@gmail.com>           +#+  +:+       +#+        */
+/*   By: jalbiser <jalbiser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 13:05:49 by jalbiser          #+#    #+#             */
-/*   Updated: 2024/07/02 11:42:41 by ekrause          ###   ########.fr       */
+/*   Updated: 2024/07/02 11:48:29 by jalbiser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -105,7 +105,7 @@ int	main(int argc, char **argv, char **envp)
 			tokens = parser(prompt, &env);
 			handler_command(tokens, &env, &cpy_pwd);
 			add_history(prompt);
-			ft_free_tokens(tokens);
+			ft_free_tokens(&tokens);
 		}
 		free(prompt);
 	}

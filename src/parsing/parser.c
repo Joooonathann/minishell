afb02e376dd021863a957c7514e5ac35eaab7608
Emeric Krause <ekrause@k0r2p3.42mulhouse.fr>
2024-08-28 15:45:52 +0200

    ft_error

--- src/parsing/parser.c
+++ src/parsing/parser.c
@@ -3,23 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jalbiser <jalbiser@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ekrause <emeric.yukii@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 12:52:19 by ekrause           #+#    #+#             */
-/*   Updated: 2024/08/27 17:40:36 by jalbiser         ###   ########.fr       */
+/*   Updated: 2024/08/28 14:43:25 by ekrause          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_error(char *str, char *token)
+void	ft_error(int count, ...)
 {
-	fprintf(stderr, "myfuckingbash: ");
-	if (str)
-		fprintf(stderr, "%s", str);
-	if (token)
-		fprintf(stderr, "`%s'", token);
-	fprintf(stderr, "\n");
+	va_list	args;
+	char	*str;
+	int		i;
+
+	str = NULL;
+	i = 0;
+	va_start(args, count);
+	while (i < count)
+	{
+		str = va_arg(args, char *);
+		write(2, str, ft_strlen(str));
+		i++;
+	}
+	write (2, "\n", 1);
+	va_end(args);
 }
 
 int	check_meta_characters(t_tokens *tokens)
@@ -29,14 +38,14 @@ int	check_meta_characters(t_tokens *tokens)
 		if (tokens->type == TYPE_REDIRECTION && (!tokens->next || tokens->next->type != TYPE_ARGUMENT))
 		{
 			if (tokens->next)
-				ft_error("syntax error near unexpected token ", tokens->next->value);
+				ft_error(5, "myfuckingbash: ", "syntax error near unexpected token ", "`", tokens->next->value, "\'");
 			else
-				ft_error("syntax error near unexpected token ", "newline");
+				ft_error(3, "myfuckingbash: ", "syntax error near unexpected token ", "newline");
 			return (0);
 		}
 		else if (tokens->type == TYPE_PIPE && (!tokens->next || tokens->next->type != TYPE_COMMAND || !tokens->prev))
 		{
-			ft_error("syntax error near unexpected token ", tokens->value);
+			ft_error(5, "myfuckingbash: ", "syntax error near unexpected token ", "`", tokens->value, "\'");
 			return (0);	
 		}
 		tokens = tokens->next;
@@ -56,6 +65,6 @@ t_tokens	*parser(char *str, t_vars **env)
 		return (NULL);
 	//parse_meta_characters(&tokens);
 	trime_useless_quotes(&tokens);
-	//ft_print_tokens(tokens);
+	ft_print_tokens(tokens);
 	return (tokens);
 }

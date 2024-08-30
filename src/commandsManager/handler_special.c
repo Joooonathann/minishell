fe1d6cd5fe586092a775aecbe2c71765a57e132d
jalbiser <jalbiser@student.42.fr>
2024-08-30 18:33:30 +0200

    update

--- src/commandsManager/handler_special.c
+++ src/commandsManager/handler_special.c
@@ -6,7 +6,7 @@
 /*   By: jalbiser <jalbiser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 17:40:37 by jalbiser          #+#    #+#             */
-/*   Updated: 2024/08/30 18:09:06 by jalbiser         ###   ########.fr       */
+/*   Updated: 2024/08/30 18:32:16 by jalbiser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -110,7 +110,7 @@ void	create_file(t_tokens **tokens, t_tokens *command)
 	}
 }
 
-t_tokens	*tokens_redirection(t_tokens **tokens)
+t_tokens	*tokens_redirection(t_tokens **tokens, t_vars **env)
 {
 	t_tokens	*current;
 	t_tokens	*new_tokens;
@@ -131,6 +131,12 @@ t_tokens	*tokens_redirection(t_tokens **tokens)
 			while (file)
 			{
 				fd = open(file->value, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+				if (fd == -1)
+				{
+					perror("open");
+					exit_code("1", env);
+					return (new_tokens);
+				}
 				if (!file->next)
 					dup2(fd, STDOUT_FILENO);
 				close (fd);
@@ -145,6 +151,12 @@ t_tokens	*tokens_redirection(t_tokens **tokens)
 			while (file)
 			{
 				fd = open(file->value, O_WRONLY | O_CREAT | O_APPEND, 0644);
+				if (fd == -1)
+				{
+					perror("open");
+					exit_code("1", env);
+					return (new_tokens);
+				}
 				if (!file->next)
 					dup2(fd, STDOUT_FILENO);
 				close (fd);
@@ -159,6 +171,12 @@ t_tokens	*tokens_redirection(t_tokens **tokens)
 			while (file)
 			{
 				fd = open(file->value, O_RDONLY);
+				if (fd == -1)
+				{
+					perror("open");
+					exit_code("1", env);
+					return (new_tokens);
+				}
 				if (!file->next)
 					dup2(fd, STDIN_FILENO);
 				close (fd);
@@ -192,66 +210,90 @@ t_tokens	*tokens_redirection(t_tokens **tokens)
 	}
 	return (new_tokens);
 }
-
-int	handler_special(t_tokens *tokens, t_vars **env, char **cpy_path)
+void handle_pipes(t_tokens **tokens_split, int i, int pipefd[2], int prev_fd)
 {
-	int			pipefd[2];
-	pid_t		pid;
-	int			i;
-	int			prev_fd;
-	t_tokens	**tokens_split;
+    if (tokens_split[i + 1])
+        dup2(pipefd[1], STDOUT_FILENO); // Setup pipe for next command
+    else if (prev_fd != -1)
+        dup2(prev_fd, STDIN_FILENO); // Connect to previous pipe
+}
 
-	i = 0;
-	prev_fd = -1;
-	tokens_split = malloc(sizeof(t_tokens *) * (count_tokens(tokens) + 1));
-	if (!tokens_split)
-		return (-1);
-	create_tokens_split(tokens_split, tokens);
-	while (tokens_split[i])
-	{
-		if (pipe(pipefd) == -1)
-		{
-			perror("pipe");
-			exit(EXIT_FAILURE);
-		}
-		pid = fork();
-		if (pid == -1)
-		{
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
-		if (pid == 0) // Processus fils
-		{
-			if (prev_fd != -1)
-			{
-				dup2(prev_fd, STDIN_FILENO);
-				close(prev_fd);
-			}
-			if (tokens_split[i + 1])
-				dup2(pipefd[1], STDOUT_FILENO);
-			close(pipefd[0]);
-			close(pipefd[1]);
-			t_tokens *new_tokens = tokens_redirection(&tokens_split[i]);
-			if (new_tokens)
-				handler_command(new_tokens, env, cpy_path);
-			else
-				handler_command(tokens_split[i], env, cpy_path);
-			new_tokens = NULL;
-			exit(EXIT_SUCCESS);
-		}
-		else // Processus père
-		{
-			close(pipefd[1]);
-			if (prev_fd != -1)
-				close(prev_fd);
-			prev_fd = pipefd[0];
-		}
-		i++;
-	}
-	for (int j = 0; j < i; j++)
-		wait(NULL);
-	if (prev_fd != -1)
-		close(prev_fd);
-	free(tokens_split);
-	return (0);
+int handler_special(t_tokens *tokens, t_vars **env, char **cpy_path)
+{
+    int pipefd[2];
+    pid_t pid;
+    int i;
+    int prev_fd;
+    t_tokens **tokens_split;
+    int status;
+    int exit_code_value = 0;
+
+    i = 0;
+    prev_fd = -1;
+    tokens_split = malloc(sizeof(t_tokens *) * (count_tokens(tokens) + 1));
+    if (!tokens_split)
+        return (-1);
+    create_tokens_split(tokens_split, tokens);
+
+    while (tokens_split[i])
+    {
+        if (pipe(pipefd) == -1)
+        {
+            perror("pipe");
+            exit(EXIT_FAILURE);
+        }
+        pid = fork();
+        if (pid == -1)
+        {
+            perror("fork");
+            exit_code("1", env);
+            exit(EXIT_FAILURE);
+        }
+        if (pid == 0)
+        {
+            if (prev_fd != -1)
+            {
+                dup2(prev_fd, STDIN_FILENO);
+                close(prev_fd);
+            }
+            handle_pipes(tokens_split, i, pipefd, prev_fd);
+            close(pipefd[0]);
+            close(pipefd[1]);
+            
+            t_tokens *new_tokens = tokens_redirection(&tokens_split[i], env);
+            if (new_tokens)
+                handler_command(new_tokens, env, cpy_path);
+            else
+                handler_command(tokens_split[i], env, cpy_path);
+            exit(EXIT_SUCCESS);
+        }
+        else
+        {
+            close(pipefd[1]);
+            if (prev_fd != -1)
+                close(prev_fd);
+            prev_fd = pipefd[0];
+        }
+        i++;
+    }
+    
+    for (int j = 0; j < i; j++)
+    {
+        waitpid(-1, &status, 0);
+        if (WIFEXITED(status))
+        {
+            exit_code_value = WEXITSTATUS(status);
+            exit_code(ft_itoa(exit_code_value), env);
+        }
+        else
+        {
+            exit_code("1", env);
+        }
+    }
+
+    if (prev_fd != -1)
+        close(prev_fd);
+    free(tokens_split);
+    
+    return (exit_code_value);
 }

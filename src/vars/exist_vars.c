da9a073a31566cd898d25365d36911482f1beccd
Jonathan Albiser <jalbiser@k2r4p4.42mulhouse.fr>
2024-09-16 16:38:19 +0200

    fix leak

--- src/vars/exist_vars.c
+++ src/vars/exist_vars.c
@@ -6,7 +6,7 @@
 /*   By: jalbiser <jalbiser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 22:32:42 by jalbiser          #+#    #+#             */
-/*   Updated: 2024/09/06 01:15:42 by jalbiser         ###   ########.fr       */
+/*   Updated: 2024/09/16 16:38:09 by jalbiser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	exist_vars(t_vars *env, char *key)
 {
 	while (env)
 	{
-		if (ft_strcmp(env->key, key))
+		if (ft_strcmp(env->key, key) && !env->mask)
 			return (1);
 		env = env->next;
 	}
